faf90479cb04efa7376a6a74a452f7e2be017f10
whiplade <nmawira13@gmail.com>
2023-06-13 20:16:32 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+//is an equation whose multiplied results will equal 62
+const num1 = 31
+const num2 = 2
+const multiply = num2 * num1
 
-// Write your code here
+//generate a random integer greater than 0
+const random = Math.floor(Math.random() * 10) + 1;
+
+//is an equation that calculates a remainder that is equal to 4
+const num3 = 67
+const num4 = 9
+const mod = num3 % num4
+
+//will return 20 as the highest number in the set
+const max = Math.max(1,19,20,18,4,2)
